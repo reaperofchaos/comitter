e3f790f92342b02682949f060c7c072404dab1b6
reaperofchaos <tokugawa111@gmail.com>
2023-07-14 17:25:37 -0400

    rally query

---
+++ Committer/Queries/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Committer
+//
+//  Created by Jacob Conner on 7/14/23.
+//
+
+import Foundation
+
+let rallyAllTaskForiteration = "https://rally1.rallydev.com/slm/webservice/v2.0/hierarchicalrequirement?query=((( Iteration.EndDate >= today) AND ( Iteration.StartDate <= today )) AND ( Project.Name = \"b.cxo.po.development tools\"))&fetch=FormattedID,Feature&pagesize=100"
+
---
+++ keys.swift
@@ -0,0 +1,10 @@
+//
+//  keys.swift
+//  Committer
+//
+//  Created by Jacob Conner on 7/14/23.
+//
+
+import Foundation
+
+let API_KEY = "_1ulzSEtIT2SPorm2TGTcdJ0jwmMy5kxhpGPXTTEiCGg"
